fdd5c0e949121655d7de9662063d088d118a8bcf
ayuko399 <ayumikonishi108@gmail.com>
2025-01-27 16:32:26 +0000

    5: add GET /api/articles endpoint

--- models/articles.models.js
+++ models/articles.models.js
@@ -3,10 +3,36 @@ const db = require("../db/connection");
 exports.selectArticleById = (article_id) => {
   const SQLString = `SELECT * FROM articles WHERE article_id = $1`;
   return db.query(SQLString, [article_id]).then(({ rows }) => {
-    console.log(rows, "<<<<<<<<<<rows");
     if (rows.length === 0) {
       return Promise.reject({ status: 404, msg: "Article not found" });
     }
     return rows[0];
+  });
+};
+
+exports.selectArticles = () => {
+  let SQLString = `
+  SELECT
+    articles.author,
+    articles.title,
+    articles.article_id,
+    articles.topic,
+    articles.created_at,
+    articles.votes,
+    articles.article_img_url,
+    COUNT (comments.comment_id) AS comment_count
+  FROM articles
+  LEFT JOIN comments ON articles.article_id = comments.article_id
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC`;
+
+  return db.query(SQLString).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "not found" });
+    }
+    return rows.map((article) => {
+      article.comment_count = Number(article.comment_count);
+      return article;
+    });
   });
 };
