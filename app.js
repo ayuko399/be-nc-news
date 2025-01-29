dab70b6017cd882e93c5d323b37480a057fd353a
ayuko399 <ayumikonishi108@gmail.com>
2025-01-29 19:12:55 +0000

    add route folder and routers for each endpoints

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ const express = require("express");
 const app = express();
 const endpoints = require("./endpoints.json");
 
-const { getTopics, getEndpoints } = require("./controllers/topics.controllers");
-const {
-  getArticleById,
-  getArticles,
-  patchArticleById,
-} = require("./controllers/articles.controllers");
-const {
-  getCommentsByArticleId,
-  postComments,
-  deleteComments,
-} = require("./controllers/comments.controllers");
-const { getUsers } = require("./controllers/users.controllers");
+const articlesRouter = require("./routes/articles.router");
+const topicsRouter = require("./routes/topics.router");
+const commentsRouter = require("./routes/comments.router");
+const usersRouter = require("./routes/uers.router");
 
 app.use(express.json());
 
@@ -21,18 +13,10 @@ app.get("/api", (req, res) => {
   res.status(200).send({ endpoints });
 });
 
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id", getArticleById);
-app.patch("/api/articles/:article_id", patchArticleById);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-app.post("/api/articles/:article_id/comments", postComments);
-app.delete("/api/comments/:comment_id", deleteComments);
-
-app.get("/api/users", getUsers);
+app.use("/api/topics", topicsRouter);
+app.use("/api/articles/", articlesRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/users", usersRouter);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Path not found" });
---
+++ routes/articles.router.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  getArticles,
+  getArticleById,
+  patchArticleById,
+} = require("../controllers/articles.controllers");
+const {
+  getCommentsByArticleId,
+  postComments,
+} = require("../controllers/comments.controllers");
+
+const articlesRouter = express.Router();
+
+articlesRouter.get("/", getArticles);
+articlesRouter.get("/:article_id", getArticleById);
+articlesRouter.patch("/:article_id", patchArticleById);
+
+articlesRouter.get("/:article_id/comments", getCommentsByArticleId);
+articlesRouter.post("/:article_id/comments", postComments);
+
+module.exports = articlesRouter;
