ebe835d62736650dd89e9d82e099c1005c66897d
ayuko399 <ayumikonishi108@gmail.com>
2025-01-30 15:28:18 +0000

    add GET /api/users/:username endpoint

--- models/users.models.js
+++ models/users.models.js
@@ -6,3 +6,13 @@ exports.selectUsers = () => {
     return rows;
   });
 };
+
+exports.selectUserByUsername = (username) => {
+  const sqlString = `SELECT * FROM users WHERE username = $1`;
+  return db.query(sqlString, [username]).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Username not found" });
+    }
+    return rows[0];
+  });
+};
