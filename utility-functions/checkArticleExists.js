95ca7796644204424683491894fa53e9da335ed3
ayuko399 <tok392870@gmail.com>
2025-02-18 18:24:53 +0000

    type in utils function

--- utility-functions/checkArticleExists.js
+++ utility-functions/checkArticleExists.js
@@ -12,7 +12,7 @@ function checkArticleExists(article_id) {
 
 function checkCommentExists(comment_id) {
   return db
-    .query(`SELECT * FROM comments WHERE article_id = $1`, [comment_id])
+    .query(`SELECT * FROM comments WHERE comment_id = $1`, [comment_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Comment not found" });
